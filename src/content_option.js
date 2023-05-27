9103f3fcb476855ac0b95f1333946c31d865448e
LAPTOP-GCFV47F1\ASUS <yusufjojo244@gmail.com>
2023-05-28 05:53:25 +0700

    pp

---
+++ src/content_option.js
@@ -0,0 +1,97 @@
+const logotext = "Yusuf J";
+const meta = {
+  title: "Yusuf Jauhar Alif Faqih",
+  description: "I’m Yusuf Jauhar Alif Faqih _ Full stack devloper,currently working in Jakarta",
+};
+
+const introdata = {
+  title: "I’m Yusuf Jauhar Alif Faqih",
+  animated: {
+    first: "Error 418: I'm a teapot, not a bug. Let's brew a solution.",
+    second: "Error 101: The programmer's mind has encountered a hiccup. Restart and refactor!",
+    third: "I web development",
+  },
+  description: "Saya seorang pengembangan situs web dengan minat khusus pada menciptakan pengalaman visual yang menarik dan fungsional.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "about my self",
+  aboutme:
+    "Saya adalah seorang front-end developer yang bersemangat dan memiliki kecintaan yang mendalam terhadap desain dan pengembangan web. Saya memiliki pengetahuan yang kuat dalam HTML, CSS, dan JavaScript, serta pengalaman dalam menciptakan antarmuka pengguna yang menarik dan responsif.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Junior Front-End",
+    where: "Freelance",
+    date: "2022",
+  },
+  {
+    jobtitle: "Junior Back-End",
+    where: "Freelance",
+    date: "2023",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 73,
+  },
+  {
+    name: "React Native",
+    value: 53,
+  },
+  {
+    name: "Node js",
+    value: 78,
+  },
+  {
+    name: "SQL",
+    value: 77,
+  },
+  {
+    name: "Mongo db",
+    value: 81,
+  },
+  {
+    name: "PHP",
+    value: 68,
+  },
+];
+
+const services = [
+  {
+    title: "Front-End",
+    description: "Menambahkan animasi dan efek interaktif ke elemen situs web untuk meningkatkan pengalaman pengguna dan memberikan kesan yang menarik.",
+  },
+  {
+    title: "Back-end",
+    description: "Membangun logika bisnis dan fungsionalitas di sisi server, seperti pemrosesan formulir, otentikasi pengguna, manajemen sesi, dan integrasi dengan API pihak ketiga.",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "yusufjojo244@gmail.com",
+  YOUR_FONE: `+62 878 3156 2908`,
+  description:
+    "Terima kasih telah mengunjungi situs kami! Jika Anda memiliki pertanyaan, komentar, atau ingin berdiskusi lebih lanjut tentang layanan kami, jangan ragu untuk menghubungi kami. Kami senang mendengar dari Anda dan siap membantu. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: `https://github.com/yusufjauhar`,
+  facebook: "https://facebook.com",
+  instagram: "https://instagram.com/jauhar.yusuf",
+  //   linkedin: "https://linkedin.com",
+  //   twitter: "https://twitter.com",
+};
+export { meta, dataabout, worktimeline, skills, services, introdata, contactConfig, socialprofils, logotext };
